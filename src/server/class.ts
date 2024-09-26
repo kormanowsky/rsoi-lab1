45dcec732dceda8692a09baecd1ec29811845019
Михаил Кормановский <kormanowsky@gmail.com>
2024-09-26 19:53:38 +0300

    добавить минимальную обработку ручек

--- src/server/class.ts
+++ src/server/class.ts
@@ -1,28 +1,64 @@
+import express from 'express';
+
 export class Server {
     constructor(port: number = 8888) {
+        this.server = express();
+        this.server.use(express.json());
+
+        this.port = port;
+        
+        this.handleGetAllRequest = this.handleGetAllRequest.bind(this);
+        this.handleGetOneRequest = this.handleGetOneRequest.bind(this);
+        this.handleCreateOneRequest = this.handleCreateOneRequest.bind(this);
+        this.handleUpdateOneRequest = this.handleUpdateOneRequest.bind(this);
+        this.handleDeleteOneRequest = this.handleDeleteOneRequest.bind(this);
+
+        this.initRoutes();
+    }
+
+    start(): void {
+        // TODO: возможно, что-то написть в callback функции
+        this.server.listen(this.port, '0.0.0.0', () => {});
+    }
+
+    stop(): void {
         
     }
 
-    start(): void {}
-    stop(): void {}
+    protected initRoutes(): void {
+        this.server.get('/api/v1/persons', this.handleGetAllRequest);
+        this.server.post('/api/v1/persons', this.handleCreateOneRequest);
+        this.server.get('/api/v1/persons/:id', this.handleGetOneRequest);
+        this.server.patch('/api/v1/persons/:id', this.handleUpdateOneRequest);
+        this.server.delete('/api/v1/persons/:id', this.handleDeleteOneRequest);
+        // TODO: роут 404?
+    }
 
-    protected handleGetAllRequest(): void {
+    protected handleGetAllRequest(req, res): void {
+        res.sendStatus(200);
 
     }
 
-    protected handleGetOneRequest(): void {
+    protected handleGetOneRequest(req, res): void {
+        res.sendStatus(200);
 
     }
 
-    protected handleCreateOneRequest(): void {
+    protected handleCreateOneRequest(req, res): void {
+        res.sendStatus(200);
 
     }
 
-    protected handleUpdateOneRequest(): void {
+    protected handleUpdateOneRequest(req, res): void {
+        res.sendStatus(200);
 
     }
 
-    protected handleDeleteOneRequest(): void {
+    protected handleDeleteOneRequest(req, res): void {
+        res.sendStatus(200);
         
     }
+
+    private server: ReturnType<typeof express>;
+    private port: number;
 }
